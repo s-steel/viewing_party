55765b2471f77593bebb9ca9f749fcfccbe6fa63
Joshua Carey <jdcarey128@gmail.com>
2020-12-01 16:16:39 -0700

    Run Users migration, test User email and password validations, include BCrypt gem

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validation' do
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:password)}
+  end
+end
