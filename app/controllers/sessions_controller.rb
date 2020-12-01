94651f829151470b1b02555353875218beb16d32
Sean Steel <seanrsteel@gmail.com>
2020-12-01 16:56:52 -0700

    Add authentication to user when created a new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,14 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.find_by(email: params[:email])
-      session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in!"
-      redirect_to '/dashboard'
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:success] = "You have successfully logged in!"
+        redirect_to '/dashboard'
+      else
+        flash[:error] = 'Invalid email or password, please try again.'
+        redirect_to root_path
+      end
     else
       flash[:error] = 'Invalid email or password, please try again.'
       redirect_to root_path
