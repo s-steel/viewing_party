ff87aedd6cca5436c5bed99e746bfaf18eb0577d
Sean Steel <seanrsteel@gmail.com>
2020-12-01 16:09:02 -0700

    Add create function in sessions controller to handle flash messages and finding user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    flash[:success] = 'You are already logged in!' if current_user
 
-  def create 
     redirect_to '/dashboard'
+  end
+
+  def create
+    if user = User.find_by(email: params[:email])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome #{user.email}!"
+      redirect_to '/dashboard'
+    else
+      flash[:error] = 'Invalid email or password, please try again.'
+      redirect_to root_path
+    end
   end
 end
