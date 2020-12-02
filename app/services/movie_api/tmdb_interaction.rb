4a72f49c25402bfb5685baf36ecf4385f4f69150
Aidan Murray <aidanhansm@gmail.com>
2020-12-01 22:18:16 -0700

    Update TMDB Interaction to include 40 results

--- app/services/movie_api/tmdb_interaction.rb
+++ app/services/movie_api/tmdb_interaction.rb
@@ -5,14 +5,22 @@ require_relative 'movie_data'
 class TMDBInteraction
   # Returns an array of MovieData objects from TMDB related to the search term
   def self.search_tmdb(query)
-    response = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
+    conn = Faraday.new('https://api.themoviedb.org/3/search/movie') do |req|
       req.params['api_key'] = ENV['TMDB_API_KEY']
       req.params['query'] = query
     end
 
-    structure = JSON.parse(response.body)
+    page1 = conn.get do |req|
+      req.params['page'] = 1
+    end
 
-    structure['results'].map do |result|
+    page2 = conn.get do |req|
+      req.params['page'] = 2
+    end
+
+    results = JSON.parse(page1.body['results']).concat(JSON.parse(page2.body['results']))
+
+    results.map do |result|
       MovieData.new(result)
     end
   end
