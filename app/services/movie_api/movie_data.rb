1006be3807d60b545836a74b2ddbc49df030e35c
Aidan Murray <aidanhansm@gmail.com>
2020-12-02 16:10:12 -0700

    Make MovieData objects read-only

--- app/services/movie_api/movie_data.rb
+++ app/services/movie_api/movie_data.rb
@@ -2,7 +2,7 @@ class MovieData
   def initialize(data)
     data.each do |key, value|
       instance_variable_set("@#{key}", value)
-      self.class.send(:attr_accessor, key)
+      self.class.send(:attr_reader, key)
     end
   end
 end
