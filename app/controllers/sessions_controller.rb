ea082318d5e3647d26d2e80c77485db93c0efd72
Sean Steel <seanrsteel@gmail.com>
2020-12-01 19:37:58 -0700

    Refactor create method within the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    if user = User.find_by(email: params[:email])
-      if user.authenticate(params[:password])
-        session[:user_id] = user.id
-        flash[:success] = 'You have successfully logged in!'
-        redirect_to '/dashboard'
-      else
-        flash[:error] = 'Invalid email or password, please try again.'
-        redirect_to root_path
-      end
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in!'
+      redirect_to '/dashboard'
     else
       flash[:error] = 'Invalid email or password, please try again.'
       redirect_to root_path
