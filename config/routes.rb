20d0c9311bf272f47c5b1169d631cfc49d0cab85
Sean Steel <seanrsteel@gmail.com>
2020-12-01 19:26:00 -0700

    Remove route helper from form with in the welcome page for user login, this simplifies it

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  post '/', to: 'sessions#create', as: :login
+  post '/', to: 'sessions#create'
 
   get '/dashboard', to: 'users#show'
 end
