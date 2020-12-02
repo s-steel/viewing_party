1df67bb61f5b9cbe950e9a7125fa58cbff2bec9d
Sean Steel <seanrsteel@gmail.com>
2020-12-01 17:11:49 -0700

    Update current_user method in the application controller to include memoization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
