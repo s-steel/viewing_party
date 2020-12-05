c41fd97baca6765701bd887903002454defb403f
Joshua Carey <jdcarey128@gmail.com>
2020-12-05 16:49:59 -0700

    Add with_friends to user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,17 @@ FactoryBot.define do
     email { Faker::Internet.email}
     password { 'password' }
     password_confirmation { 'password' }
+
+    trait :with_friends do  
+      transient do
+        friend_count { 3 }
+      end
+
+      after(:create) do |user, evaluator|
+        friends = create_list(:user, evaluator.friend_count)
+        user.followers << friends
+        user.followed << friends
+      end
+    end 
   end
 end
