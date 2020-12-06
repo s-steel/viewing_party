79302a4d41b11f660da2eeb583e1925632777ef0
Joshua Carey <jdcarey128@gmail.com>
2020-12-06 10:41:39 -0700

    Define friendship controller create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,15 +1,25 @@
 class FriendshipsController < ApplicationController
   
   def create 
-    require 'pry'; binding.pry
+    if User.find_user(friend_params) && !friend_self?(friend_params)
+      current_user.followed << User.find_user(friend_params)
+    elsif User.find_user(friend_params)
+      flash[:error] = 'You cannot add yourself as a friend'
+    else 
+      flash[:error] = 'That user doesn\'t exist in the system'
+    end
     redirect_to dashboard_path
   end
 
 
   private 
 
+  def friend_self?(friend)
+    current_user.email.include?(friend)
+  end
+
   def friend_params
-    params.permit(:friend_search)
+    params.permit(:friend_search)[:friend_search]
   end
   
 end
