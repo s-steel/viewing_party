06b5857c96a79842d7bbe6a2c91ac17d32902def
Aidan Murray <aidanhansm@gmail.com>
2020-12-07 19:12:28 -0700

    Add sad paths for new viewing party page spec

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -24,5 +24,27 @@ describe 'New viewing party' do
       expect(party.duration).to eq(95)
       expect(party.start_date_time).to eq('Friday January 1st, 2021 at 11:30 AM')
     end
+
+    it 'bad date and time' do
+      visit viewing_party_new_path(movie_id: 400_160)
+
+      expect(page).to have_content('The SpongeBob Movie')
+      fill_in('viewing_party[date]', with: 'fsdafasdf')
+      fill_in('viewing_party[time]', with: 'asdfadfs')
+
+      click_button('Create Party')
+
+      expect(page).to have_content('Please fill out all fields with proper information')
+    end
+
+    it 'no date and time' do
+      visit viewing_party_new_path(movie_id: 400_160)
+
+      expect(page).to have_content('The SpongeBob Movie')
+
+      click_button('Create Party')
+
+      expect(page).to have_content('Please fill out all fields with proper information')
+    end
   end
 end
