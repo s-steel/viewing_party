641ce1eb0569c9a10f8e06d8686cce7465f53c4c
Sean Steel <seanrsteel@gmail.com>
2020-12-07 18:28:19 -0700

    Edit test for a user to log out and be redirected to the welcome page, test passing

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -3,15 +3,20 @@ require 'rails_helper'
 describe 'As a user that is logged in' do
   describe 'I can log out' do
     it 'I click logout and am returned to the welcome page' do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit dashboard_path
-      expect(page).to have_content("Welcome #{user.user_name}!")
-      click_button 'Logout'
+      @user = User.create!(email: 'me@email.com',
+        password: 'password', 
+        password_confirmation: 'password')
+
       visit root_path
+      
+      fill_in 'email', with: "#{@user.email}"
+      fill_in 'password', with: "#{@user.password}"
+      click_button 'Sign In'
+      expect(page).to have_content("Welcome #{@user.user_name}!")
+      click_button 'Logout'
       expect(current_path).to eq(root_path)
       expect(page).to have_content('You are now logged out!')
-      expect(page).to_not have_content("Welcome #{user.user_name}!")
+      expect(page).to_not have_content("Welcome #{@user.user_name}!")
     end
   end
 end
