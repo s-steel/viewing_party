c4da94e05e0a0c0bdcac0ffa91a223cbb8f3784b
Aidan Murray <aidanhansm@gmail.com>
2020-12-07 20:01:21 -0700

    Update feature spec for viewing parties

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -5,46 +5,52 @@ describe 'New viewing party' do
     before :each do
       @user = create(:user)
       @user2 = create(:user)
-      # @user.followed << @user2
+      @user.followed << @user2
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
-    it 'without followers without dashboard view' do
-      visit viewing_party_new_path(movie_id: 400_160)
+    it 'good path' do
+      VCR.use_cassette('viewing-party/new_spec_good') do
+        visit viewing_party_new_path(movie_id: 400_160)
 
-      expect(page).to have_content('The SpongeBob Movie')
-      fill_in('viewing_party[date]', with: '01/01/2021')
-      fill_in('viewing_party[time]', with: '11:30')
+        expect(page).to have_content('The SpongeBob Movie')
+        fill_in('viewing_party[date_time]', with: '01/01/2021 11:30')
 
-      click_button('Create Party')
+        check(@user2.user_name)
 
-      party = Parties.last
+        click_button('Create Party')
 
-      expect(party.movie_id).to eq(400_160)
-      expect(party.duration).to eq(95)
-      expect(party.start_date_time).to eq('Friday January 1st, 2021 at 11:30 AM')
+        party = Parties.last
+
+        expect(party.movie_id).to eq(400_160)
+        expect(party.duration).to eq(95)
+        expect(party.start_date_time).to eq('Friday January 1st, 2021 at 11:30 AM')
+      end
     end
 
     it 'bad date and time' do
-      visit viewing_party_new_path(movie_id: 400_160)
+      VCR.use_cassette('viewing-party/new_spec_bad_date_time') do
+        visit viewing_party_new_path(movie_id: 400_160)
 
-      expect(page).to have_content('The SpongeBob Movie')
-      fill_in('viewing_party[date]', with: 'fsdafasdf')
-      fill_in('viewing_party[time]', with: 'asdfadfs')
+        expect(page).to have_content('The SpongeBob Movie')
+        fill_in('viewing_party[date_time]', with: 'sdflkadsjflkjaf')
 
-      click_button('Create Party')
+        click_button('Create Party')
 
-      expect(page).to have_content('Please fill out all fields with proper information')
+        expect(page).to have_content('Please fill out all fields with proper information')
+      end
     end
 
     it 'no date and time' do
-      visit viewing_party_new_path(movie_id: 400_160)
+      VCR.use_cassette('viewing-party/new_spec_no_date_time') do
+        visit viewing_party_new_path(movie_id: 400_160)
 
-      expect(page).to have_content('The SpongeBob Movie')
+        expect(page).to have_content('The SpongeBob Movie')
 
-      click_button('Create Party')
+        click_button('Create Party')
 
-      expect(page).to have_content('Please fill out all fields with proper information')
+        expect(page).to have_content('Please fill out all fields with proper information')
+      end
     end
   end
 end
