044821068ca0c93659e96c41dc543c1b11326127
Joshua Carey <jdcarey128@gmail.com>
2020-12-09 17:35:27 -0700

    Build out seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'fake@example.com', 
-            password: 'password', 
-            password_confirmation: 'password')
+
+PartyGuest.destroy_all
+Party.destroy_all 
+Friendship.destroy_all
+User.destroy_all
+
+fake, user, example = User.create(
+            [
+              {
+                email: 'fake_user@example.com', 
+                password: 'fake_user', 
+                password_confirmation: 'fake_user'
+              },
+              {
+                email: 'user@example.com', 
+                password: 'user', 
+                password_confirmation: 'user'
+              },
+              {
+                email: 'example_user@example.com', 
+                password: 'example_user', 
+                password_confirmation: 'example_user'
+              }
+            ]
+          )
+
+fake.followed << [user, example]
+fake.followers << [user, example]
+example.followed << [user]
+example.followers << [user]
+
+party, exclusive_party = Party.create(
+                                      [
+                                        {
+                                          movie_id: 121,
+                                          duration: 179,
+                                          when: DateTime.now,
+                                          host_id: fake.id 
+                                        },
+                                        {
+                                          movie_id: 5966,
+                                          duration: 90,
+                                          when: DateTime.now,
+                                          host_id: example.id
+                                        }
+                                      ]
+                                    )
+
+party.guests << [user, example]
+exclusive_party.guests << user 
