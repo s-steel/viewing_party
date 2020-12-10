c8af0790a888346aeef03fa4ef55ca1b454e586a
Sean Steel <seanrsteel@gmail.com>
2020-12-10 14:03:18 -0700

    Format facade movie details

--- app/facades/movie_search_facade.rb
+++ app/facades/movie_search_facade.rb
@@ -11,10 +11,12 @@ class MovieSearchFacade
 
   def self.movie_details(movie_id, similar_limit = 100)
     json_response = TMDBInteraction.movie_details(movie_id)
+
     movie = MovieData.new(json_response.except(:reviews, :credits, :similar))
     actors = create_actors(json_response[:credits][:cast]).first(10)
     reviews = create_reviews(json_response[:reviews][:results])
     similar_movies = create_movie_data(json_response[:similar][:results]).take(similar_limit)
+
     { movie: movie, cast: actors, reviews: reviews, similar_movies: similar_movies }
   end
 
